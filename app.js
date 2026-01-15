713919225bc8a8173bc73196ed73adcaacd1687b
zeronneai <zeronne.ai@gmail.com>
2026-01-15 09:42:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -34,3 +34,20 @@ function appendMessage(author, text) {
   chatMessages.appendChild(div);
   chatMessages.scrollTop = chatMessages.scrollHeight;
 }
+// SCROLL REVEAL
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+reveals.forEach(el => observer.observe(el));
